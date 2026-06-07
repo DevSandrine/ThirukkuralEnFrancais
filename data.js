487dbb18e372ac177c1092cc17d806d5580f3ec0
Sandrine Dev <devaradjas@gmail.com>
2026-06-07 16:21:45 +0200

    save local work

---
+++ data.js
@@ -0,0 +1,14 @@
+const thirukuralData = {
+    sections: {
+        "1": { name: "Aram", chapters: [
+            { id: 1, name: "Louange à Dieu", kurals: [
+                { id: 1, tamil: "அகர முதல எழுத்தெல்லாம் ஆதி\nபகவன் முதற்றே உலகு.", translation: "De même que la lettre A est au commencement de toutes les lettres, de même l'Éternel Dieu est au commencement du monde." },
+                { id: 2, tamil: "கற்றதனால் ஆய பயனென்கொல் வாலறிவன்\nநற்றாள் தொழாஅர் எனின்.", translation: "À quoi sert la science à celui qui ne s'incline pas aux pieds de Celui qui possède la connaissance pure ?" }
+                // Les autres kurals...
+            ]},
+            { id: 2, name: "L'importance de la pluie", kurals: [] }
+        ]},
+        "2": { name: "Porul", chapters: [] },
+        "3": { name: "Inbam", chapters: [] }
+    }
+};
