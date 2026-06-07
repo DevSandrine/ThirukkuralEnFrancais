9e0d73ca6181ce524b9586352a9bb2c6700caf35
DevSandrine <149612201+DevSandrine@users.noreply.github.com>
2026-06-07 19:26:37 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,44 +1,65 @@
 const thirukuralData = {
   1: {
     name: "Arattupāl",
+    fr: "Livre de la Vertu",
+
     iyals: [
       {
         id: 1,
         name: "Payiraviyal",
-        fr:" Préliminaires",
+        fr: "Préliminaires",
+
         chapters: [
           {
             id: 1,
             name: "Kadavul Vaazhthu",
             fr: "Louange à Dieu",
+
             kurals: [
               {
-               id: 1,
-               tamil: "அகர முதல எழுத்தெல்லாம் ஆதி பகவன் முதற்றே உலகு",
-               tamilfr: " Akara Mudhala Ezhuththellaam Aadhi Pakavan Mudhatre Ulaku", 
-               translation: "Comme la lettre « A » est la première de toutes les lettres de l’alphabet,de même, le Dieu éternel est le premier dans l’univers.",
-               comment: " Ce premier vers du Thirukkural exprime l’idée que, tout comme l’alphabet commence par la première lettre « A », le monde et toute existence trouvent leur origine dans le divin."
+                id: 1,
+
+                tamil:
+                  "அகர முதல எழுத்தெல்லாம் ஆதி பகவன் முதற்றே உலகு",
+
+                transliteration:
+                  "Akara Mudhala Ezhuththellaam Aadhi Pakavan Mudhatre Ulaku",
+
+                translation:
+                  "Comme la lettre « A » est la première de toutes les lettres de l'alphabet, de même, le Dieu éternel est le premier dans l'univers.",
+
+                explanation:
+                  "Ce premier vers du Thirukkural exprime l'idée que, tout comme l'alphabet commence par la lettre « A », le monde et toute existence trouvent leur origine dans le divin."
               },
 
-
-
-              
-              { id: 2, tamil: "...", translation: "..." }
+              {
+                id: 2,
+                tamil: "...",
+                transliteration: "...",
+                translation: "...",
+                explanation: "..."
+              }
             ]
           },
+
           {
             id: 2,
-            name: "Blessing of Rain",
+            name: "Vaan Sirappu",
+            fr: "Grandeur de la pluie",
             kurals: []
           },
+
           {
             id: 3,
-            name: "Greatness of Ascetics",
+            name: "Neethaar Perumai",
+            fr: "Grandeur des ascètes",
             kurals: []
           },
+
           {
             id: 4,
-            name: "Strength of Virtue",
+            name: "Aran Valiyuruththal",
+            fr: "Importance de la vertu",
             kurals: []
           }
         ]
@@ -48,11 +69,13 @@ const thirukuralData = {
 
   2: {
     name: "Porutpāl",
+    fr: "Livre de la Société",
     iyals: []
   },
 
   3: {
     name: "Inbattupāl",
+    fr: "Livre de l'Amour",
     iyals: []
   }
 };
